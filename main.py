15942a90fcb0e53939862d9888d8ab49d3226133
william <william16.lim@gmail.com>
2024-08-21 22:19:18 +1000

    feat: list of words

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import json
 from ui.window import App
 
 
--- ui/window.py
+++ ui/window.py
@@ -1,24 +1,47 @@
 import customtkinter
 
 
+class List(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+        self.labels = []
+        self.grid_columnconfigure(0, weight=1)
+
+    def update_list(self, characters):
+        for label in self.labels:
+            label.destroy()
+        self.labels.clear()
+
+        for i, char in enumerate(characters):
+            label = customtkinter.CTkLabel(self, text=char)
+            label.grid(row=i, column=0, sticky="we")
+            self.labels.append(label)
+
+
 class App(customtkinter.CTk):
     def __init__(self):
         super().__init__()
 
+        self.characters = []
         self.title("my app")
-        self.geometry("400x150")
+        self.geometry("1280x720")
         self.grid_columnconfigure((0, 1), weight=1)
 
         self.button = customtkinter.CTkButton(
-            self, text="my button", command=self.button_callback)
-        self.button.grid(row=0, column=0, padx=20, pady=20,
-                         sticky="ew", columnspan=2)
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="checkbox 1")
-        self.checkbox_1.grid(row=1, column=0, padx=20,
-                             pady=(0, 20), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="checkbox 2")
-        self.checkbox_2.grid(row=1, column=1, padx=20,
-                             pady=(0, 20), sticky="w")
+            self, text="my button", command=self.add_character)
+        self.button.grid(row=0, column=1, padx=20, pady=20,
+                         sticky="nsew", columnspan=1)
+        self.input = customtkinter.CTkEntry(master=self)
+        self.input.grid_configure(
+            row=0, column=0, padx=20, pady=20, sticky="nsew")
 
-    def button_callback(self):
-        print("button pressed")
+        self.list = List(self)
+        self.list.grid_configure(
+            row=1, column=0, sticky="we", columnspan=2)
+
+    def add_character(self):
+        new_character = self.input.get()
+        if new_character:
+            self.characters.append(new_character)
+            self.list.update_list(self.characters)
+            self.input.delete(0, 'end')
